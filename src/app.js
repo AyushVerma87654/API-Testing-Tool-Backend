5fbdba2fd45b1a516cbe0a1d256e6e511167a60f
Ayush Verma <tgmcodeyogi@gmail.com>
2025-12-09 23:59:26 +0530

    Edited Frontend Deployed Url

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["https://api-testing-tool.netlify.app/", "http://localhost:5173"],
+    origin: ["https://api-testing-tool.netlify.app", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["content-type", "authorization"],
